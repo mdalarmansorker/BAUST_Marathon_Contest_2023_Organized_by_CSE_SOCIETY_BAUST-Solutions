8d2b00c0c94d208bff24277c2c5e2d969ab01920
MD Al Arman Sorker <73396847+mdalarmansorker@users.noreply.github.com>
2023-09-22 18:12:04 +0600

    Create Z.c

---
+++ Z.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int  a,i;
+    for(i=0;i>=0;i++)
+    {
+                scanf("%d",&a);
+                 if(a==42)
+            break;
+            printf("%d\n",a);
+    }
+    return 0;
+}
